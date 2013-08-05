0173464b0fdce4a52ef34dad92de97bba46ba63e
Carles Climent <carlescliment@gmail.com>
2013-08-05 13:44:50 +0200

    Changed MocoBuilder to ApplicationBuilder

--- config/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$loader = require dirname(__FILE__) . '/../vendor/autoload.php';
-
-// Register the directory to your include files
-$loader->add('', __DIR__ . '/../src');
-$loader->add('', __DIR__ . '/../tests');
