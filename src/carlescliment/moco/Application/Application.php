69f36517288bc2167b3f41aa238546ffd0862c33
Carles Climent <carlescliment@gmail.com>
2013-08-13 09:40:31 +0200

    Allow the addition of extensions

--- src/carlescliment/moco/Application/Application.php
+++ src/carlescliment/moco/Application/Application.php
@@ -3,22 +3,29 @@
 namespace carlescliment\moco\Application;
 
 
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface,
+    Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 use carlescliment\moco\Exception\MocoException;
 
 class Application extends Moco
 {
     private $compiler_passes = array();
+    private $extensions = array();
     private $running = false;
 
 
     public function addCompilerPass(CompilerPassInterface $compiler_pass)
     {
-        if ($this->running) {
-            throw new MocoException('You cannot add a compiler to a running application.');
-        }
+        $this->assertNotRunning();
         $this->compiler_passes[] = $compiler_pass;
+    }
+
+
+    public function addExtension(ExtensionInterface $extension)
+    {
+        $this->assertNotRunning();
+        $this->extensions[] = $extension;
     }
 
 
@@ -26,6 +33,7 @@ class Application extends Moco
     {
         $this->assertContainerIsSet();
         $this->addCompilerPassesToContainer();
+        $this->addExtensionsToContainer();
         $this->container->compile();
         $this->running = true;
     }
@@ -38,10 +46,27 @@ class Application extends Moco
         }
     }
 
+
+    private function assertNotRunning()
+    {
+        if ($this->running) {
+            throw new MocoException('You cannot add a compiler to a running application.');
+        }
+    }
+
+
     private function addCompilerPassesToContainer()
     {
         foreach ($this->compiler_passes as $compiler_pass) {
             $this->container->addCompilerPass($compiler_pass);
+        }
+    }
+
+
+    private function addExtensionsToContainer()
+    {
+        foreach ($this->extensions as $extension) {
+            $this->container->registerExtension($extension);
         }
     }
 }
--- tests/moco/Tests/Application/ApplicationTest.php
+++ tests/moco/Tests/Application/ApplicationTest.php
@@ -50,12 +50,31 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase
     }
 
 
+    /**
+     * @test
+     */
+    public function itAllowsRegisteringANewExtension()
+    {
+        // Arrange
+        $container = $this->createApplicationContainer();
+        $extension = $this->getMock('Symfony\Component\DependencyInjection\Extension\ExtensionInterface');
+        $this->app->addExtension($extension);
+
+        // Expect
+        $container->expects($this->once())
+            ->method('registerExtension')
+            ->with($extension);
+
+        // Act
+        $this->app->run();
+    }
+
 
     /**
      * @test
      * @expectedException carlescliment\moco\Exception\MocoException
      */
-    public function itRaisesAnErrorWhenAddingACompilerPassWhenRunning()
+    public function itRaisesAnErrorWhenAddingACompilerToARunningApplication()
     {
         // Arrange
         $container = $this->createApplicationContainer();
@@ -67,6 +86,25 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase
         // Act
         $this->app->addCompilerPass($compiler_pass);
     }
+
+
+    /**
+     * @test
+     * @expectedException carlescliment\moco\Exception\MocoException
+     */
+    public function itRaisesAnErrorWhenAddingAnExtensionToARunningApplication()
+    {
+        // Arrange
+        $container = $this->createApplicationContainer();
+        $this->app->setContainer($container);
+        $this->app->run();
+        $extension = $this->getMock('Symfony\Component\DependencyInjection\Extension\ExtensionInterface');
+        
+        // Expect
+        // Act
+        $this->app->addExtension($extension);
+    }
+
 
     /**
      * @test
